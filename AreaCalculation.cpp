85b11a63de0515211ae5759b7ab2869cdbff35f9
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-02-25 19:17:25 +0000

    added AreaCalculation

---
+++ AreaCalculation.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Andrew-Ten-Den All rights reserved
+//
+// Created by: Andrew-Ten-Den
+// Created on: February 2022
+// This program calculates the area and perimeter of a rectangle
+//    with dimensions 5cm by 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions: " << std::endl;
+    std::cout << "5cm by 3cm" << std::endl;
+    std::cout << std::endl;
+    std::cout << "The Area is " << (5 * 3) << "cm²." << std::endl;
+    std::cout << "The Perimeter is " << ((5 + 3) * 2) << "cm." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Problem Solved" << std::endl;
+}
