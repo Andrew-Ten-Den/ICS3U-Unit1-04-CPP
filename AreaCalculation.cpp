471d16e1d0a1566be7ee3998dbe37a5c6f94759d
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-03-01 13:44:28 +0000

    fixed Area Calculation

--- AreaCalculation.cpp
+++ AreaCalculation.cpp
@@ -9,10 +9,10 @@
 
 int main() {
     std::cout << "If a rectangle has the dimensions: " << std::endl;
-    std::cout << "5cm by 3cm" << std::endl;
+    std::cout << "5 cm by 3 cm" << std::endl;
     std::cout << std::endl;
-    std::cout << "The Area is " << (5 * 3) << "cm²." << std::endl;
-    std::cout << "The Perimeter is " << ((5 + 3) * 2) << "cm." << std::endl;
+    std::cout << "The Area is " << (5 * 3) << " cm²." << std::endl;
+    std::cout << "The Perimeter is " << ((5 + 3) * 2) << " cm." << std::endl;
     std::cout << std::endl;
-    std::cout << "Problem Solved" << std::endl;
+    std::cout << "Done" << std::endl;
 }
